772c955d76c438e1acf24070dd858345c1ce6c62
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-11-21 12:44:14 +0900

    for pr 1922

--- eccube3/clone-root.sh
+++ eccube3/clone-root.sh
@@ -3,3 +3,9 @@
 cd /var
 rm -r /var/www
 git clone --depth=50 -b ${ECCUBE_BRANCH} ${ECCUBE_REPOS} ${ECCUBE_PATH}
+
+cd ${ECCUBE_PATH}
+git fetch origin pull/1922/head:pull_1922
+git checkout pull_1922
+
+
