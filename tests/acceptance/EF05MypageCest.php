f2ad04a2f7006ad471f1a6ef0daa4ca81a2a17d3
lqdung <lqdung@lockon-vn.com>
2017-03-28 10:06:05 +0700

    Debug 4

--- tests/acceptance/EF05MypageCest.php
+++ tests/acceptance/EF05MypageCest.php
@@ -29,7 +29,7 @@ class EF05MypageCest
 
         // お気に入り登録
         $I->amOnPage('/products/detail/2');
-        $I->see('パーコレーター', '#main_middle #detail_description_box__name');
+        $I->see('パーコレーター', '#main_middle #item_detail #detail_wrap #item_detail_area h3');
         $I->click('#favorite');
 
         $I->amOnPage('/mypage');
